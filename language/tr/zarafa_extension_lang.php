aaa7a3749fd00ecb6091641fb71e037e42e0b511
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:17:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Uzantısı';
+$lang['zarafa_extension_app_description'] = 'Zarafa Uzatma açıklaması'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Hesaplar Uzantısı';
+$lang['zarafa_extension_administrator_privileges'] = 'yönetici Ayrıcalıkları';
+$lang['zarafa_extension_hard_quota'] = 'sert Kota';
+$lang['zarafa_extension_account'] = 'hesap';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Hesap bayrağı geçersiz.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Yönetici bayrağı geçersiz.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Sabit kota geçersiz.';
+$lang['zarafa_extension_quota_override'] = 'kota Geçersiz Kıl';
+$lang['zarafa_extension_soft_quota'] = 'yazılım Alıntı';
+$lang['zarafa_extension_warning_quota'] = 'uyarı Kotası';
