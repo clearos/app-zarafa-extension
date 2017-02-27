c72584aef387e3632ebf3900f748790becbb6b0c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:12:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'ການຂະຫຍາຍ Zarafa';
+$lang['zarafa_extension_app_description'] = 'ລາຍລະອຽດ Zarafa Extension'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa ບັນຊີການຂະຫຍາຍ';
+$lang['zarafa_extension_administrator_privileges'] = 'ສິດທິພິເສດ Administrator';
+$lang['zarafa_extension_hard_quota'] = 'Quota ຍາກ';
+$lang['zarafa_extension_account'] = 'ບັນຊີ';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'ທຸງຊາດບັນຊີບໍ່ຖືກຕ້ອງ.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'ທຸງຊາດບໍລິຫານແມ່ນບໍ່ຖືກຕ້ອງ.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'ໂຄຕາການຍາກທີ່ບໍ່ຖືກຕ້ອງ.';
+$lang['zarafa_extension_quota_override'] = 'ແທນໂກຕາ';
+$lang['zarafa_extension_soft_quota'] = 'ອ້າງຊອບແວ';
+$lang['zarafa_extension_warning_quota'] = 'Quota ການເຕືອນໄພ';
