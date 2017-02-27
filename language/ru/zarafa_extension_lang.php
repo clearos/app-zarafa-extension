fa47f739b7ec252088b35b3f0807e0d71a628f61
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:15:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Описание Zarafa Extension'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa счетов Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'привилегии администратора';
+$lang['zarafa_extension_hard_quota'] = 'Квота';
+$lang['zarafa_extension_account'] = 'Счет';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Флаг счета недействительно.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'флаг администратора является недействительным.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Квота является недействительным.';
+$lang['zarafa_extension_quota_override'] = 'Квота Override';
+$lang['zarafa_extension_soft_quota'] = 'программное обеспечение Цитировать';
+$lang['zarafa_extension_warning_quota'] = 'Внимание Квота';
