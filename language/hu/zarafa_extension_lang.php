a01f40bb06516d0ea6ec14abe88a45eb0ddb6244
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:10:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension leírás'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa számlák kiterjesztése';
+$lang['zarafa_extension_administrator_privileges'] = 'rendszergazdai jogosultságok';
+$lang['zarafa_extension_hard_quota'] = 'kemény kvóta';
+$lang['zarafa_extension_account'] = 'Fiók';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Fiók zászló érvénytelen.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator zászló érvénytelen.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Kemény kvóta érvénytelen.';
+$lang['zarafa_extension_quota_override'] = 'kvóta felülírása';
+$lang['zarafa_extension_soft_quota'] = 'szoftver Idézet';
+$lang['zarafa_extension_warning_quota'] = 'Figyelmeztetés kvóta';
