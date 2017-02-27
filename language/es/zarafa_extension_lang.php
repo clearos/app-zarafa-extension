cc76a15d32eb4a9e8bb0e5a84b75205e2c8fe6bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:16:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extensión';
+$lang['zarafa_extension_app_description'] = 'Descripción Zarafa Extensión'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Cuentas Extensión';
+$lang['zarafa_extension_administrator_privileges'] = 'Privilegios de administrador';
+$lang['zarafa_extension_hard_quota'] = 'cuota por hardware';
+$lang['zarafa_extension_account'] = 'Cuenta';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'bandera de cuenta no es válido.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'bandera de administrador no es válido.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'cuota de disco duro no es válido.';
+$lang['zarafa_extension_quota_override'] = 'Anulación de cuotas';
+$lang['zarafa_extension_soft_quota'] = 'Cita de software';
+$lang['zarafa_extension_warning_quota'] = 'cuota de advertencia';
