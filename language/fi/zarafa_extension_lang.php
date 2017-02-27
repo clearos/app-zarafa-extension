f162284b23093eb1ff38cb0285d6a01c4f00fc53
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:09:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension kuvaus'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa tilit Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'järjestelmänvalvojan oikeudet';
+$lang['zarafa_extension_hard_quota'] = 'Hard kiintiö';
+$lang['zarafa_extension_account'] = 'Tili';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Tilin lippu ei kelpaa.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Ylläpitäjä lippu ei kelpaa.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Kova kiintiö on virheellinen.';
+$lang['zarafa_extension_quota_override'] = 'kiintiö ohitus';
+$lang['zarafa_extension_soft_quota'] = 'ohjelmisto Quote';
+$lang['zarafa_extension_warning_quota'] = 'Varoitus kiintiö';
