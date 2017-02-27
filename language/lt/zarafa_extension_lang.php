c56979ef104992e1b4cd39b2a8b36147194d5636
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:12:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa pratęsimas';
+$lang['zarafa_extension_app_description'] = 'Zarafa pratęsimas aprašymas'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa sąskaitos išplėtimas';
+$lang['zarafa_extension_administrator_privileges'] = 'administratoriaus privilegijomis';
+$lang['zarafa_extension_hard_quota'] = 'sunku kvota';
+$lang['zarafa_extension_account'] = 'sąskaita';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Sąskaita vėliava yra neteisinga.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administratorius vėliava yra neteisinga.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Sunku kvota yra neteisinga.';
+$lang['zarafa_extension_quota_override'] = 'kvota Valdymas';
+$lang['zarafa_extension_soft_quota'] = 'programinė įranga Citata';
+$lang['zarafa_extension_warning_quota'] = 'Įspėjimas kvota';
