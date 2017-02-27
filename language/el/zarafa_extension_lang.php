d24cf3552e2e7f758076c9a0241d0beb541c1d09
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:09:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Επέκταση';
+$lang['zarafa_extension_app_description'] = 'Περιγραφή Zarafa Επέκταση'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Λογαριασμοί Επέκταση';
+$lang['zarafa_extension_administrator_privileges'] = 'Προνόμια διαχειριστή';
+$lang['zarafa_extension_hard_quota'] = 'σκληρό Ποσόστωση';
+$lang['zarafa_extension_account'] = 'Λογαριασμός';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'σημαία του λογαριασμού δεν είναι έγκυρο.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'σημαία διαχειριστή δεν είναι έγκυρο.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Σκληρό ποσόστωση είναι άκυρη.';
+$lang['zarafa_extension_quota_override'] = 'ποσόστωση Παράκαμψη';
+$lang['zarafa_extension_soft_quota'] = 'παράθεση λογισμικού';
+$lang['zarafa_extension_warning_quota'] = 'προειδοποίηση Ποσόστωση';
