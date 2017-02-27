1e1ae8b5ad8efb6c58d6660cfc0da3a04731ea89
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:11:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa ұзарту';
+$lang['zarafa_extension_app_description'] = 'Zarafa Кеңейтім сипаттамасы'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Есеп ұзарту';
+$lang['zarafa_extension_administrator_privileges'] = 'әкімші құқықтары';
+$lang['zarafa_extension_hard_quota'] = 'қатты квота';
+$lang['zarafa_extension_account'] = 'шот';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Шот туы жарамсыз болып табылады.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Әкімші туы жарамсыз болып табылады.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Қатты квота жарамсыз болып табылады.';
+$lang['zarafa_extension_quota_override'] = 'квота Override';
+$lang['zarafa_extension_soft_quota'] = 'бағдарламалық қамтамасыз ету дәйексөз';
+$lang['zarafa_extension_warning_quota'] = 'Ескерту квота';
