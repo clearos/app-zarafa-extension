59f6df27cd707b9c0a9135ad18b9ea200e68e5fe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:13:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Toronga';
+$lang['zarafa_extension_app_description'] = 'Whakaahuatanga Zarafa Toronga'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Accounts Toronga';
+$lang['zarafa_extension_administrator_privileges'] = 'Painga kaiwhakahaere';
+$lang['zarafa_extension_hard_quota'] = 'Quota Hard';
+$lang['zarafa_extension_account'] = 'pūkete';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'He muhu haki Pūkete.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'He muhu Kaiwhakahaere kara.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'He muhu roherohenga Hard.';
+$lang['zarafa_extension_quota_override'] = 'Whakaawe Quota';
+$lang['zarafa_extension_soft_quota'] = 'Quote pūmanawa';
+$lang['zarafa_extension_warning_quota'] = 'Quota whakatūpato';
