0b10b06dad0ff712e8b7e44a69bb9bcbfe508047
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:14:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'ها Zarafa فرمت';
+$lang['zarafa_extension_app_description'] = 'توضیحات ها Zarafa فرمت'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'ها Zarafa حساب فرمت';
+$lang['zarafa_extension_administrator_privileges'] = 'دسترسی مدیر';
+$lang['zarafa_extension_hard_quota'] = 'سهمیه سخت';
+$lang['zarafa_extension_account'] = 'حساب';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'پرچم حساب نامعتبر است.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'پرچم مدیر نامعتبر است.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'سهمیه سخت نامعتبر است.';
+$lang['zarafa_extension_quota_override'] = 'نادیده گرفتن سهمیه';
+$lang['zarafa_extension_soft_quota'] = 'نرم افزار نقل قول';
+$lang['zarafa_extension_warning_quota'] = 'سهمیه هشدار';
