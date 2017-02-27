8358963ce1eb1efda44097b06f3834ed00c566cc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:17:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Опис Zarafa Extension'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa рахунків Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'привілеї адміністратора';
+$lang['zarafa_extension_hard_quota'] = 'квота';
+$lang['zarafa_extension_account'] = 'рахунки';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Прапор рахунку недійсне.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'прапор адміністратора є недійсним.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Квота є недійсним.';
+$lang['zarafa_extension_quota_override'] = 'квота Override';
+$lang['zarafa_extension_soft_quota'] = 'програмне забезпечення Цитувати';
+$lang['zarafa_extension_warning_quota'] = 'Увага Квота';
