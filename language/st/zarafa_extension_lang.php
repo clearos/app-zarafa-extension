e42f73d08c9452578111e47c21a4d55286fcb280
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:15:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension tlhaloso'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Litlaleho Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'mohlokomeli oa Litokelo';
+$lang['zarafa_extension_hard_quota'] = 'Hard palo';
+$lang['zarafa_extension_account'] = 'Account';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Account folakha ha o sebetse.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Mohlokomeli oa folakha ha o sebetse.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard palo ha o sebetse.';
+$lang['zarafa_extension_quota_override'] = 'palo hatelle';
+$lang['zarafa_extension_soft_quota'] = 'Software Quote';
+$lang['zarafa_extension_warning_quota'] = 'temoso ea palo';
