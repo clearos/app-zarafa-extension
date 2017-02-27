cfbc25804b2f6ee8dd3c976cdb513f294c4c34c2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:18:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Mô tả Zarafa Extension'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Tài khoản mở rộng';
+$lang['zarafa_extension_administrator_privileges'] = 'Ưu đãi quản trị viên';
+$lang['zarafa_extension_hard_quota'] = 'Hạn ngạch cứng';
+$lang['zarafa_extension_account'] = 'Tài khoản';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'cờ tài khoản không hợp lệ.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Quản trị viên cờ là không hợp lệ.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'hạn ngạch cứng là không hợp lệ.';
+$lang['zarafa_extension_quota_override'] = 'Hạn ngạch Override';
+$lang['zarafa_extension_soft_quota'] = 'phần mềm Trích dẫn';
+$lang['zarafa_extension_warning_quota'] = 'Cảnh báo hạn ngạch';
