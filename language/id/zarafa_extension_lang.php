9ce4fa2cf843319aca9c1cfef4765231da0ef5ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:10:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Ekstensi';
+$lang['zarafa_extension_app_description'] = 'deskripsi Zarafa Ekstensi'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Akun Ekstensi';
+$lang['zarafa_extension_administrator_privileges'] = 'Wewenang administrator';
+$lang['zarafa_extension_hard_quota'] = 'keras Kuota';
+$lang['zarafa_extension_account'] = 'Rekening';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Akun bendera tidak valid.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator bendera tidak valid.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Keras kuota tidak valid.';
+$lang['zarafa_extension_quota_override'] = 'kuota Override';
+$lang['zarafa_extension_soft_quota'] = 'software Kutipan';
+$lang['zarafa_extension_warning_quota'] = 'Kuota peringatan';
