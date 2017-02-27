0dfb0aec1cf3978da6716732d247c8b52132d4f1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:07:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension նկարագրությունը'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa հաշիվներ Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'Ադմինիստրատոր Արտոնություններ';
+$lang['zarafa_extension_hard_quota'] = 'Hard Սահմանը';
+$lang['zarafa_extension_account'] = 'հաշիվ';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Հաշվի դրոշը անվավեր է:';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator դրոշը անվավեր է:';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard քվոտան է անվավեր:';
+$lang['zarafa_extension_quota_override'] = 'քվոտա Վերագրել';
+$lang['zarafa_extension_soft_quota'] = 'ծրագրային Մեջբերում';
+$lang['zarafa_extension_warning_quota'] = 'Զգուշացում Սահմանը';
