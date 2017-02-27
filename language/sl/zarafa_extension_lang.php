071cb0cba8f3cbd3fbe0dd82b2219003f33a3e8f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:15:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Razširitev';
+$lang['zarafa_extension_app_description'] = 'Opis Zarafa Razširitev'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa računi Razširitev';
+$lang['zarafa_extension_administrator_privileges'] = 'Administrator Privilegiji';
+$lang['zarafa_extension_hard_quota'] = 'Hard Kvota';
+$lang['zarafa_extension_account'] = 'račun';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'zastava računa je neveljavna.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator zastava je neveljavna.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard kvota je neveljavna.';
+$lang['zarafa_extension_quota_override'] = 'kvota Override';
+$lang['zarafa_extension_soft_quota'] = 'programska oprema Citiraj';
+$lang['zarafa_extension_warning_quota'] = 'Opozorilo Kvota';
