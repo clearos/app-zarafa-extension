89f1d83b990c4678f1b136fb992c36405793a1e4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:18:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'incazelo Zarafa Extension'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Accounts Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'Administrator Amalungelo';
+$lang['zarafa_extension_hard_quota'] = 'Quota Hard';
+$lang['zarafa_extension_account'] = '-Akhawunti';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'flag-Akhawunti ayivumelekile.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator flag ayivumelekile.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'quota Hard ayivumelekile.';
+$lang['zarafa_extension_quota_override'] = 'Quota Dlova';
+$lang['zarafa_extension_soft_quota'] = 'isofthiwe Quote';
+$lang['zarafa_extension_warning_quota'] = 'Isexwayiso Quota';
