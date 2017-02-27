cd9135152489b7623cc9f97c5e19c68b4ebdd7a7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:11:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension nkọwa'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Accounts Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'Administrator Ùgwù';
+$lang['zarafa_extension_hard_quota'] = 'ike oke';
+$lang['zarafa_extension_account'] = 'akaụntụ';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Account ọkọlọtọ bụ ghara ịdị irè.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator ọkọlọtọ bụ ghara ịdị irè.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Ike oke bụ ghara ịdị irè.';
+$lang['zarafa_extension_quota_override'] = 'oke akpagbu';
+$lang['zarafa_extension_soft_quota'] = 'software Quote';
+$lang['zarafa_extension_warning_quota'] = 'ịdọ aka ná ntị oke';
