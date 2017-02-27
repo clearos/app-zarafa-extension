c03655e2a0ff83ae6527b39161411ccd51b43ead
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:11:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension lýsing'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa reikningar Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'stjórnandi Forréttindi';
+$lang['zarafa_extension_hard_quota'] = 'Hard Kvóti';
+$lang['zarafa_extension_account'] = 'Reikningur';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Reikningur fáninn er ógilt.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Stjórnandi fáninn er ógilt.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard kvóta er ógilt.';
+$lang['zarafa_extension_quota_override'] = 'kvóti Hunsa';
+$lang['zarafa_extension_soft_quota'] = 'hugbúnaður Quote';
+$lang['zarafa_extension_warning_quota'] = 'Viðvörun Kvóti';
