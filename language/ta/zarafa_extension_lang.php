76211ec94f37b9ce3539a407dbc185c872ecc441
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:16:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa நீட்டிப்பு';
+$lang['zarafa_extension_app_description'] = 'Zarafa நீட்டிப்பு விளக்கம்'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa கணக்குகள் நீட்டிப்பு';
+$lang['zarafa_extension_administrator_privileges'] = 'நிர்வாகி உரிமைகளுடன்';
+$lang['zarafa_extension_hard_quota'] = 'ஹார்ட் ஒதுக்கீடு';
+$lang['zarafa_extension_account'] = 'கணக்கு';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'கணக்கு கொடி தவறானது.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'நிர்வாகி கொடி தவறானது.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'ஹார்ட் ஒதுக்கீடு தவறானது.';
+$lang['zarafa_extension_quota_override'] = 'ஒதுக்கீடு மீறு';
+$lang['zarafa_extension_soft_quota'] = 'மென்பொருள் ஆனால்';
+$lang['zarafa_extension_warning_quota'] = 'எச்சரிக்கை ஒதுக்கீடு';
