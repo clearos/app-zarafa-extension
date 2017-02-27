9e438efcb0bf469e587138a0fb4807860bd8cb0a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:10:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa એક્સ્ટેંશન';
+$lang['zarafa_extension_app_description'] = 'Zarafa એક્સ્ટેન્શન વર્ણન'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa એકાઉન્ટ્સ એક્સ્ટેંશન';
+$lang['zarafa_extension_administrator_privileges'] = 'સંચાલક વિશેષાધિકારો';
+$lang['zarafa_extension_hard_quota'] = 'હાર્ડ ક્વોટા';
+$lang['zarafa_extension_account'] = 'એકાઉન્ટ';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'એકાઉન્ટ ધ્વજ અમાન્ય છે.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'સંચાલક ધ્વજ અમાન્ય છે.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'હાર્ડ ક્વોટા અમાન્ય છે.';
+$lang['zarafa_extension_quota_override'] = 'ક્વોટા પર ફરીથી લખો';
+$lang['zarafa_extension_soft_quota'] = 'સોફ્ટવેર ભાવ';
+$lang['zarafa_extension_warning_quota'] = 'ચેતવણી ક્વોટા';
