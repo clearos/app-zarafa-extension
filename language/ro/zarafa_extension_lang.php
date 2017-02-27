9d86ba7ab39a655ca9733ff51be7fae640c875c7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:14:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extensie';
+$lang['zarafa_extension_app_description'] = 'Descriere Zarafa Extensie'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Conturi de extensie';
+$lang['zarafa_extension_administrator_privileges'] = 'privilegii de administrator';
+$lang['zarafa_extension_hard_quota'] = 'greu Cotă';
+$lang['zarafa_extension_account'] = 'Cont';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'pavilion cont nu este valid.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administratorul de pavilion este nevalid.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'contingent greu este nevalid.';
+$lang['zarafa_extension_quota_override'] = 'cotă Suprascriere';
+$lang['zarafa_extension_soft_quota'] = 'software Citat';
+$lang['zarafa_extension_warning_quota'] = 'Cotă de avertizare';
