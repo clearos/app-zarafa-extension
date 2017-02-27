fd571acf615399fe347dce52a781bbafa04b1ad3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:18:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'זאַראַפאַ עקסטענסיאָן';
+$lang['zarafa_extension_app_description'] = 'זאַראַפאַ געשפּרייט באַשרייַבונג'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'זאַראַפאַ אַקקאָונץ עקסטענסיאָן';
+$lang['zarafa_extension_administrator_privileges'] = 'אַדמיניסטראַטאָר פּריווילעגעס';
+$lang['zarafa_extension_hard_quota'] = 'שווער קוואָטאַ';
+$lang['zarafa_extension_account'] = 'באַריכטן';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'אַקאַונט פאָן איז פאַרקריפּלט.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'אַדמיניסטראַטאָר פאָן איז פאַרקריפּלט.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'שווער קוואָטע איז פאַרקריפּלט.';
+$lang['zarafa_extension_quota_override'] = 'קוואָטע אָוועררידע';
+$lang['zarafa_extension_soft_quota'] = 'ווייכווארג ציטירן';
+$lang['zarafa_extension_warning_quota'] = 'ווארענונג קוואָטאַ';
