8b7eaad46fc99218cc6ba97dacb74f20208bf759
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:10:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Síneadh Zarafa';
+$lang['zarafa_extension_app_description'] = 'Síneadh Zarafa síos'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Extension Cuntais';
+$lang['zarafa_extension_administrator_privileges'] = 'Pribhléidí riarthóir';
+$lang['zarafa_extension_hard_quota'] = 'Cuóta crua';
+$lang['zarafa_extension_account'] = 'Cuntas';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Is bratach Cuntas neamhbhailí.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Is bratach Riarthóir neamhbhailí.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Is cuóta crua neamhbhailí.';
+$lang['zarafa_extension_quota_override'] = 'Sáraigh cuóta';
+$lang['zarafa_extension_soft_quota'] = 'bogearraí Athfhriotail';
+$lang['zarafa_extension_warning_quota'] = 'Cuóta rabhadh';
