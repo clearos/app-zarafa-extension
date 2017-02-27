49a0b32eb07b3fe8a4768c6677c929542a6058d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:15:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension description'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Accounts Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'Mudnaanta Administrator';
+$lang['zarafa_extension_hard_quota'] = 'kootada Hard';
+$lang['zarafa_extension_account'] = 'Account';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'calanka Account waa baadil.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'calanka Maamulaha waa baadil.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'kootada adag waa baadil.';
+$lang['zarafa_extension_quota_override'] = 'tirtirto kootada';
+$lang['zarafa_extension_soft_quota'] = 'Quote software';
+$lang['zarafa_extension_warning_quota'] = 'kootada Digniin';
