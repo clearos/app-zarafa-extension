71d30dd06cd1a37ca6802fca91e3436232a5eedb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:18:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Estyniad';
+$lang['zarafa_extension_app_description'] = 'Disgrifiad Zarafa Estyniad'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Cyfrifon Zarafa Estyniad';
+$lang['zarafa_extension_administrator_privileges'] = 'Breintiau Gweinyddwr';
+$lang['zarafa_extension_hard_quota'] = 'Cwota caled';
+$lang['zarafa_extension_account'] = 'cyfrif';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'baner cyfrif yn annilys.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'baner Gweinyddwr yn annilys.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'cwota caled yn annilys.';
+$lang['zarafa_extension_quota_override'] = 'Diystyru cwota';
+$lang['zarafa_extension_soft_quota'] = 'Dyfyniad meddalwedd';
+$lang['zarafa_extension_warning_quota'] = 'Cwota rhybudd';
