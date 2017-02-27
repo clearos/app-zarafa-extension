61d2c6746730ff02a01e47d6dd7393ebce58f2d6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:13:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Estensjoni';
+$lang['zarafa_extension_app_description'] = 'Deskrizzjoni Zarafa Estensjoni'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Kontijiet Estensjoni';
+$lang['zarafa_extension_administrator_privileges'] = 'Privileġġi amministratur';
+$lang['zarafa_extension_hard_quota'] = 'kwota iebsa';
+$lang['zarafa_extension_account'] = 'kont';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'bandiera kont huwa invalidu.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'bandiera Amministratur huwa invalidu.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'kwota iebsa hija invalida.';
+$lang['zarafa_extension_quota_override'] = 'Jwarrab kwota';
+$lang['zarafa_extension_soft_quota'] = 'Quote softwer';
+$lang['zarafa_extension_warning_quota'] = 'kwota twissija';
