f5f8eb9313c296f395c66df29008d83ef93ff79a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:14:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extensão';
+$lang['zarafa_extension_app_description'] = 'A inscrição Zarafa Extensão'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Contas Extensão';
+$lang['zarafa_extension_administrator_privileges'] = 'privilégios de administrador';
+$lang['zarafa_extension_hard_quota'] = 'Quota disco';
+$lang['zarafa_extension_account'] = 'Conta';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'bandeira da conta é inválido.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'flag Administrador é inválido.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'cota máxima é inválido.';
+$lang['zarafa_extension_quota_override'] = 'Substituir quota';
+$lang['zarafa_extension_soft_quota'] = 'Citar software';
+$lang['zarafa_extension_warning_quota'] = 'Quota de advertência';
