76f51734a15b72d15b9bd521e133f4ac2fdc012a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:11:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa拡張';
+$lang['zarafa_extension_app_description'] = 'Zarafa拡張の説明'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafaアカウント拡張';
+$lang['zarafa_extension_administrator_privileges'] = '管理者権限';
+$lang['zarafa_extension_hard_quota'] = 'ハードクォータ';
+$lang['zarafa_extension_account'] = 'アカウント';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'アカウントフラグが無効です。';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = '管理者のフラグが無効です。';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'ハードクォータは無効です。';
+$lang['zarafa_extension_quota_override'] = 'クォータのオーバーライド';
+$lang['zarafa_extension_soft_quota'] = 'ソフトウェアの引用';
+$lang['zarafa_extension_warning_quota'] = '警告クォータ';
