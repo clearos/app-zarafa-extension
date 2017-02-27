111553ae912eceebcaedebf5802e6e0ff1ff8d78
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:12:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension Famaritana'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Kaonty Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'Administrator Tombontsoa';
+$lang['zarafa_extension_hard_quota'] = 'Hard anjaranareo';
+$lang['zarafa_extension_account'] = 'Account';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Account sainam-pirenena dia tsy manan-kery.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator sainam-pirenena dia tsy manan-kery.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard anjaranareo no tsy manan-kery.';
+$lang['zarafa_extension_quota_override'] = 'anjaranareo handresy ny';
+$lang['zarafa_extension_soft_quota'] = 'rindrambaiko Quote';
+$lang['zarafa_extension_warning_quota'] = 'fampitandremana anjaranareo';
