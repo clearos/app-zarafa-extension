22e3787d1c0afd4f4d930b351ffb36285ea162fe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:07:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa التمديد';
+$lang['zarafa_extension_app_description'] = 'وصف Zarafa التمديد'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa الحسابات التمديد';
+$lang['zarafa_extension_administrator_privileges'] = 'امتيازات المسؤول';
+$lang['zarafa_extension_hard_quota'] = 'حصة الصعبة';
+$lang['zarafa_extension_account'] = 'الحساب';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'علم الحساب غير صالح.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'علم المسؤول غير صالح.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'الحصة النسبية الثابت غير صالحة.';
+$lang['zarafa_extension_quota_override'] = 'الحصة تجاوز';
+$lang['zarafa_extension_soft_quota'] = 'البرنامج اقتباس';
+$lang['zarafa_extension_warning_quota'] = 'حصة تحذير';
