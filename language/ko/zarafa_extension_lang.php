d7728a5ffbf4b0d3515a512de257b087b7b88576
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:12:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa 확장';
+$lang['zarafa_extension_app_description'] = 'Zarafa 확장 설명'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa 계정 확장';
+$lang['zarafa_extension_administrator_privileges'] = '관리자 권한';
+$lang['zarafa_extension_hard_quota'] = '하드 할당량';
+$lang['zarafa_extension_account'] = '계정';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = '계정 플래그가 잘못되었습니다.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = '관리자 플래그가 잘못되었습니다.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = '하드 할당량이 잘못되었습니다.';
+$lang['zarafa_extension_quota_override'] = '할당량 재정의';
+$lang['zarafa_extension_soft_quota'] = '소프트웨어 견적';
+$lang['zarafa_extension_warning_quota'] = '경고 할당량';
