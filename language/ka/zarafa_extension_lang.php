1012b54531ab7a26eba36e06a281b6f278b924e8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:11:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa გაფართოება';
+$lang['zarafa_extension_app_description'] = 'Zarafa გაფართოება აღწერა'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa ანგარიშები გაფართოება';
+$lang['zarafa_extension_administrator_privileges'] = 'ადმინისტრატორის პრივილეგიები';
+$lang['zarafa_extension_hard_quota'] = 'Hard კვოტა';
+$lang['zarafa_extension_account'] = 'ანგარიშის';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'ანგარიშის დროშა არასწორია.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator დროშა არასწორია.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard კვოტა არასწორია.';
+$lang['zarafa_extension_quota_override'] = 'კვოტების გადააჭარბე';
+$lang['zarafa_extension_soft_quota'] = 'პროგრამული Quote';
+$lang['zarafa_extension_warning_quota'] = 'გაფრთხილება კვოტა';
