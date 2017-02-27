f416a6a2eea274a9165c5bd9e5b70acaadba31ac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:09:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Pligrandigo';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension priskribo'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Kontoj Pligrandigo';
+$lang['zarafa_extension_administrator_privileges'] = 'administranto Privilegioj';
+$lang['zarafa_extension_hard_quota'] = 'hard Kotizo';
+$lang['zarafa_extension_account'] = 'konto';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Konto flago estas nevalida.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administranto flago estas nevalida.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard kvoto estas nevalida.';
+$lang['zarafa_extension_quota_override'] = 'kvoto Override';
+$lang['zarafa_extension_soft_quota'] = 'programaro Citaĵo';
+$lang['zarafa_extension_warning_quota'] = 'Averto Kotizo';
