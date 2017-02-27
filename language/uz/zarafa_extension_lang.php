6b6a6f49e74ece29f5a100c2d962b507168c58e8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:17:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension tavsifi'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Hisob Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'Administrator imtiyozlar';
+$lang['zarafa_extension_hard_quota'] = 'hard kvotasi';
+$lang['zarafa_extension_account'] = 'Account';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Account bayrog&#39;i haqiqiy emas.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator bayrog&#39;i haqiqiy emas.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard kvota haqiqiy emas.';
+$lang['zarafa_extension_quota_override'] = 'kvota bekor qil';
+$lang['zarafa_extension_soft_quota'] = 'dasturiy ta&#39;minot Iqtibos';
+$lang['zarafa_extension_warning_quota'] = 'ogohlantirish kvotasi';
