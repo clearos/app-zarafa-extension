4eed28e6512789e08933aa87953322708c795d6d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:12:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa extensio';
+$lang['zarafa_extension_app_description'] = 'Zarafa extensio Description'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Ratio Zarafa extensio';
+$lang['zarafa_extension_administrator_privileges'] = 'Administrator privilegiorum';
+$lang['zarafa_extension_hard_quota'] = 'ferreus-orig.doc';
+$lang['zarafa_extension_account'] = 'account';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Vexillum causa nulla est.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator vexillum est invalidum.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Iuxta numerum, invalidum est.';
+$lang['zarafa_extension_quota_override'] = 'quota Override';
+$lang['zarafa_extension_soft_quota'] = 'Quote software';
+$lang['zarafa_extension_warning_quota'] = 'Quota Warning';
