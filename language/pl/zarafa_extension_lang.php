73c9e387c17ea091034f5effc8b62e477dec5244
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:14:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Opis Zarafa Extension'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Konta Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'Uprawnienia administratora';
+$lang['zarafa_extension_hard_quota'] = 'twardy Kontyngent';
+$lang['zarafa_extension_account'] = 'Konto';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Flaga konta jest nieprawidłowy.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Flaga administratora jest nieprawidłowy.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Twardy kontyngent jest nieprawidłowy.';
+$lang['zarafa_extension_quota_override'] = 'kontyngent Zastąp';
+$lang['zarafa_extension_soft_quota'] = 'oprogramowanie Cytat';
+$lang['zarafa_extension_warning_quota'] = 'Kontyngent Ostrzeżenie';
