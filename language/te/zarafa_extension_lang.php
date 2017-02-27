34915eeb332c07f183989c3070b9bc8963c8b155
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:16:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa పొడిగింపు';
+$lang['zarafa_extension_app_description'] = 'Zarafa పొడిగింపు వివరణ'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa అకౌంట్స్ పొడిగింపు';
+$lang['zarafa_extension_administrator_privileges'] = 'నిర్వాహక అధికారాలను';
+$lang['zarafa_extension_hard_quota'] = 'హార్డ్ కోటా';
+$lang['zarafa_extension_account'] = 'ఖాతా';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'ఖాతా జెండా చెల్లదు.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'వ్యవస్థ పాలకుని జెండా చెల్లదు.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'హార్డ్ కోటా చెల్లదు.';
+$lang['zarafa_extension_quota_override'] = 'కోటా భర్తీ';
+$lang['zarafa_extension_soft_quota'] = 'సాఫ్ట్వేర్ కోట్';
+$lang['zarafa_extension_warning_quota'] = 'హెచ్చరిక కోటా';
