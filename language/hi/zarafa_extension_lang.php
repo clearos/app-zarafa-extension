3bdbcc87491271bfc4e3013fa16465d304c5bb1c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:10:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa एक्सटेंशन';
+$lang['zarafa_extension_app_description'] = 'Zarafa एक्सटेंशन का विवरण'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa लेखा एक्सटेंशन';
+$lang['zarafa_extension_administrator_privileges'] = 'प्रबंधक के फ़ायदे';
+$lang['zarafa_extension_hard_quota'] = 'हार्ड कोटा';
+$lang['zarafa_extension_account'] = 'लेखा';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'खाते का झंडा अमान्य है।';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'प्रशासक झंडा अमान्य है।';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'हार्ड कोटा अमान्य है।';
+$lang['zarafa_extension_quota_override'] = 'कोटा अवहेलना';
+$lang['zarafa_extension_soft_quota'] = 'सॉफ्टवेयर उद्धरण';
+$lang['zarafa_extension_warning_quota'] = 'चेतावनी कोटा';
