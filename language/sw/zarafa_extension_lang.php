20a35113ab2a1228fa655439021214bf064d909c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:16:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa ugani';
+$lang['zarafa_extension_app_description'] = 'Zarafa ugani maelezo'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Hesabu za ugani';
+$lang['zarafa_extension_administrator_privileges'] = 'msimamizi Haki';
+$lang['zarafa_extension_hard_quota'] = 'Hard Viti Maalum';
+$lang['zarafa_extension_account'] = 'akaunti';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Akaunti bendera ni batili.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Msimamizi bendera ni batili.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard viti maalum ni batili.';
+$lang['zarafa_extension_quota_override'] = 'Viti Maalum override';
+$lang['zarafa_extension_soft_quota'] = 'programu Quote';
+$lang['zarafa_extension_warning_quota'] = 'onyo Viti Maalum';
