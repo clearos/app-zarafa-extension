502b158bf094d33006bc32901baae5f49086c67f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:11:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'ផ្នែកបន្ថែម Zarafa';
+$lang['zarafa_extension_app_description'] = 'ការរៀបរាប់ផ្នែកបន្ថែម Zarafa'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'ផ្នែកបន្ថែមគណនី Zarafa';
+$lang['zarafa_extension_administrator_privileges'] = 'សិទ្ធិគ្រប់គ្រង';
+$lang['zarafa_extension_hard_quota'] = 'កូតារឹង';
+$lang['zarafa_extension_account'] = 'គណនី';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'ទង់ជាតិគណនីមិនត្រឹមត្រូវ។';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'ទង់ជាតិអ្នកគ្រប់គ្រងមិនត្រឹមត្រូវ។';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'កូតារឹងមិនត្រឹមត្រូវ។';
+$lang['zarafa_extension_quota_override'] = 'បដិសេធកូតា';
+$lang['zarafa_extension_soft_quota'] = 'សម្រង់ផ្នែកទន់';
+$lang['zarafa_extension_warning_quota'] = 'កូតាការព្រមាន';
