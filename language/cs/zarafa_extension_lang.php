673d717b23a9198c06c23cdbe81a4ffef1c418b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:08:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Popis Zarafa Extension'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa účty Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'administrátorskými právy';
+$lang['zarafa_extension_hard_quota'] = 'Hard Kvóta';
+$lang['zarafa_extension_account'] = 'Účet';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'vlajka účtu je neplatný.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'vlajka správce je neplatný.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard kvóta je neplatný.';
+$lang['zarafa_extension_quota_override'] = 'kvóta Přepis';
+$lang['zarafa_extension_soft_quota'] = 'software Citace';
+$lang['zarafa_extension_warning_quota'] = 'Varování Kvóta';
