51d4b385d5d0894ed8077385112c7be9c4563920
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:08:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa扩展';
+$lang['zarafa_extension_app_description'] = 'Zarafa扩展描述'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa账户扩展';
+$lang['zarafa_extension_administrator_privileges'] = '管理员权限';
+$lang['zarafa_extension_hard_quota'] = '硬配额';
+$lang['zarafa_extension_account'] = '帐户';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = '帐户标志是无效的。';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = '管理员标记为无效。';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = '硬配额是无效的。';
+$lang['zarafa_extension_quota_override'] = '配额覆盖';
+$lang['zarafa_extension_soft_quota'] = '软件报价';
+$lang['zarafa_extension_warning_quota'] = '警告配额';
