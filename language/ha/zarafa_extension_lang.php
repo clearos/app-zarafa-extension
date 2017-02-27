cc9f0011e9f45611791636587f8252e943bc44b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:10:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Tsawo';
+$lang['zarafa_extension_app_description'] = 'Zarafa Tsawo description'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Accounts Tsawo';
+$lang['zarafa_extension_administrator_privileges'] = 'Administrator gata';
+$lang['zarafa_extension_hard_quota'] = 'Hard Ƙa&#39;idar';
+$lang['zarafa_extension_account'] = 'account';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Account flag ba daidai ba ne.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator flag ba daidai ba ne.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard keɓaɓɓen yawa ba daidai ba ne.';
+$lang['zarafa_extension_quota_override'] = 'Ƙa&#39;idar override';
+$lang['zarafa_extension_soft_quota'] = 'software Quote';
+$lang['zarafa_extension_warning_quota'] = 'gargadi Ƙa&#39;idar';
