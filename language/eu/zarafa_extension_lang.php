0185a4a40f57b073037ca03aef178019695a4edb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:07:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension azalpena'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Kontuak Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'administratzaile eskubideak';
+$lang['zarafa_extension_hard_quota'] = 'Hard Kupoari';
+$lang['zarafa_extension_account'] = 'kontua';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Kontua ez da baliozkoa.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator bandera baliogabea da.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard-kuota baliogabea da.';
+$lang['zarafa_extension_quota_override'] = 'Kupoari Override';
+$lang['zarafa_extension_soft_quota'] = 'software Aipamena';
+$lang['zarafa_extension_warning_quota'] = 'Abisua Kupoari';
