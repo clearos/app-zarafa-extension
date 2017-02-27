9b8790d4e9a3d8217a02ba5780c365e6178e9fd5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:16:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension Beskrivning'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Accounts Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'administratörsbehörighet';
+$lang['zarafa_extension_hard_quota'] = 'hårt Kvot';
+$lang['zarafa_extension_account'] = 'konto';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Konto flagga är ogiltig.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administratör flaggan är ogiltig.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hård kvoten är ogiltig.';
+$lang['zarafa_extension_quota_override'] = 'kvot~~POS=TRUNC';
+$lang['zarafa_extension_soft_quota'] = 'programvara Citat';
+$lang['zarafa_extension_warning_quota'] = 'varnings Kvot';
