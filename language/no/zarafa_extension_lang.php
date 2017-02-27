157371df7a76d52c9b7686082775d2b93959774f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:14:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension beskrivelse'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Kontoer Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'Administratorrettigheter';
+$lang['zarafa_extension_hard_quota'] = 'Hard Kvote';
+$lang['zarafa_extension_account'] = 'Konto';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Konto flagg er ugyldig.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator flagg er ugyldig.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard kvote er ugyldig.';
+$lang['zarafa_extension_quota_override'] = 'kvote overstyring';
+$lang['zarafa_extension_soft_quota'] = 'programvare Quote';
+$lang['zarafa_extension_warning_quota'] = 'Advarsel Kvote';
