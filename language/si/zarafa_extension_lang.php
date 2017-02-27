e3599d57c9cda64d4d76138f0d8b8bc15953560d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:15:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa ව්යාප්ති';
+$lang['zarafa_extension_app_description'] = 'Zarafa ව්යාප්ති විස්තරයක්'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa ගිණුම් ව්යාප්ති';
+$lang['zarafa_extension_administrator_privileges'] = 'පරිපාලක වරප්රසාද';
+$lang['zarafa_extension_hard_quota'] = 'Hard පංගු';
+$lang['zarafa_extension_account'] = 'ගිණුම';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'ගිණුම ධජය වලංගු නැත.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'පරිපාලක ධජය වලංගු නැත.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard කෝටා වලංගු නැත.';
+$lang['zarafa_extension_quota_override'] = 'කෝටා ජීවය';
+$lang['zarafa_extension_soft_quota'] = 'මෘදුකාංග Quote';
+$lang['zarafa_extension_warning_quota'] = 'අවවාදයයි පංගු';
