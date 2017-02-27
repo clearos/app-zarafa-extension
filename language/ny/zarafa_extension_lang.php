a9f3658f6cc8a31de032f6a25b79bac644c29575
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:08:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Extension';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension kufotokoza'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Nkhani Extension';
+$lang['zarafa_extension_administrator_privileges'] = 'woyang&#39;anira Maudindo';
+$lang['zarafa_extension_hard_quota'] = 'zovuta kuumba';
+$lang['zarafa_extension_account'] = 'nkhani';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Nkhani mbendera opanda pake.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Woyang&#39;anira mbendera opanda pake.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Zovuta kuumba ndi opanda pake.';
+$lang['zarafa_extension_quota_override'] = 'kuumba kunyalanyaza';
+$lang['zarafa_extension_soft_quota'] = 'software Mawu';
+$lang['zarafa_extension_warning_quota'] = 'chenjezo kuumba';
