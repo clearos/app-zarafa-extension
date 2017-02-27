86d25997ce69c8bb218cb93887dbb2fda272089a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:17:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Тамдиди';
+$lang['zarafa_extension_app_description'] = 'Шарҳи Zarafa Тамдиди'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa ҳисоб Тамдиди';
+$lang['zarafa_extension_administrator_privileges'] = 'Имтиёзҳои Administrator';
+$lang['zarafa_extension_hard_quota'] = 'квотаи Хард';
+$lang['zarafa_extension_account'] = 'Ҳисоби';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Парчами Ҳисоби нодуруст аст.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Парчами маъмур нодуруст аст.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'квотаи Hard нодуруст аст.';
+$lang['zarafa_extension_quota_override'] = 'ҷойгузор квотаи';
+$lang['zarafa_extension_soft_quota'] = 'нармафзори Quote';
+$lang['zarafa_extension_warning_quota'] = 'квотаи Огоҳӣ';
