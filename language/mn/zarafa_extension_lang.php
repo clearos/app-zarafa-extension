fc5d97019bc7837e0e06458ad0fa51a2133fff56
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:13:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa Өргөтгөл';
+$lang['zarafa_extension_app_description'] = 'Zarafa Өргөтгөл тодорхойлолт'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa бүртгэл Өргөтгөл';
+$lang['zarafa_extension_administrator_privileges'] = 'Админ эрх';
+$lang['zarafa_extension_hard_quota'] = 'Хатуу Ноогдлын';
+$lang['zarafa_extension_account'] = 'Дансны';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Дансны туг хүчин төгөлдөр бус байна.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administrator туг хүчин төгөлдөр бус байна.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Хатуу квот хүчин төгөлдөр бус байна.';
+$lang['zarafa_extension_quota_override'] = 'ноогдлын хэрэгсэхгүй болгож';
+$lang['zarafa_extension_soft_quota'] = 'програм хангамж Quote';
+$lang['zarafa_extension_warning_quota'] = 'Warning Ноогдлын';
