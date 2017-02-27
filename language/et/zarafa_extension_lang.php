d4f1f14f8dc7af1b7d90465884cd380271aee12c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:09:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa pikendamine';
+$lang['zarafa_extension_app_description'] = 'Zarafa Extension kirjeldus'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa kontod pikendamine';
+$lang['zarafa_extension_administrator_privileges'] = 'administraatori õigusi';
+$lang['zarafa_extension_hard_quota'] = 'karm piirang';
+$lang['zarafa_extension_account'] = 'Konto';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'Konto lipp on kehtetu.';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'Administraator lipp on kehtetu.';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'Hard kvoot on kehtetu.';
+$lang['zarafa_extension_quota_override'] = 'kvoot alistamine';
+$lang['zarafa_extension_soft_quota'] = 'tarkvara Quote';
+$lang['zarafa_extension_warning_quota'] = 'Hoiatus kvoot';
