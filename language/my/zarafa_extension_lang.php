cc13aef337f7afcc539446972455e63799ee65ca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 12:08:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/zarafa_extension_lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['zarafa_extension_app_name'] = 'Zarafa တိုးချဲ့မှု';
+$lang['zarafa_extension_app_description'] = 'Zarafa တိုးချဲ့မှုဖော်ပြချက်'; // FIXME
+$lang['zarafa_extension_zarafa_accounts_extension'] = 'Zarafa Accounts ကိုတိုးချဲ့';
+$lang['zarafa_extension_administrator_privileges'] = 'အုပ်ချုပ်ရေးမှူးအခွင့်ထူး';
+$lang['zarafa_extension_hard_quota'] = 'hard ခွဲတမ်း';
+$lang['zarafa_extension_account'] = 'အကောင့်';
+
+$lang['zarafa_extension_account_flag_is_invalid'] = 'အကောင့်အလံမမှန်ကန်ဘူး။';
+$lang['zarafa_extension_administrator_flag_is_invalid'] = 'အုပ်ချုပ်ရေးမှူးအလံမမှန်ကန်ဘူး။';
+
+$lang['zarafa_extension_hard_quota_is_invalid'] = 'hard ခွဲတမ်းမမှန်ကန်ဘူး။';
+$lang['zarafa_extension_quota_override'] = 'ခွဲတမ်းရပ်တန့်ရန်';
+$lang['zarafa_extension_soft_quota'] = 'software ကို Quote';
+$lang['zarafa_extension_warning_quota'] = 'သတိပေးခွဲတမ်း';
